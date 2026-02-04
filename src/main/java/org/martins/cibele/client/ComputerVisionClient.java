e985bd84a6d39b145b783342b910ad9d02f879f0
CibeleMartinsDev <cibelemdev@gmail.com>
2026-02-04 10:08:30 -0300

    feat: add method sendImage

--- src/main/java/org/martins/cibele/client/ComputerVisionClient.java
+++ src/main/java/org/martins/cibele/client/ComputerVisionClient.java
@@ -1,20 +1,27 @@
 package org.martins.cibele.client;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.HeaderParam;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.martins.cibele.domain.ImageAnalysisRequest;
 
-@RegisterRestClient(configKey = "ccv-client")
-@Consumes(MediaType.MULTIPART_FORM_DATA)
+
+@RegisterRestClient(configKey = "cv-client")
+@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @ConfigProperty(name = "")
 public interface ComputerVisionClient {
 
     @POST
-    String sendImageForClassificaiton(byte[] imageBytes, @HeaderParam("Prediction-Key") String predictionKey, @HeaderParam("Content-Type") String contentType);
+    @Path("/imageanalysis:analyze")
+    String sendImage(  @QueryParam("features") String features,
+                       @QueryParam("model-name") String modelName,
+                       @QueryParam("language") String language,
+                       @QueryParam("smartcrops-aspect-ratios") String smartCropsAspectRatios,
+                       @QueryParam("gender-neutral-caption") Boolean genderNeutralCaption,
+                       @QueryParam("api-version") String apiVersion,
+                       @HeaderParam("Authorization") String auth,
+                       ImageAnalysisRequest body);
+
 }
